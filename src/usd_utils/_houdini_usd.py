5e344a8f2a830940212fbaf52788e5ebfc41abfe
K.Maev <maevskayakrisfx@gmai.com>
2024-03-24 22:53:46 +0100

    Rewritten redesigned functions

--- src/usd_utils/_houdini_usd.py
+++ src/usd_utils/_houdini_usd.py
@@ -1,5 +1,6 @@
 import os
 import hou
+import json
 
 
 scheme = {
@@ -14,125 +15,11 @@ scheme = {
 class KitBashGeometryImport:
     def __init__(self):
         self.stage_path = "stage/"
+        self.metadata  = os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets_metadata.json")
+
         pass
 
     # gather materials from geometry
-
-    def get_material_data(self, node):
-
-        pack_all = node.createOutputNode("pack")  # thsi is why pack node is created twice
-        pack_all.parm("packbyname").set(True)
-        pack_all.parm("nameattribute").set("shop_materialpath")
-
-        pack_all.parm("transfer_attributes").set("shop_materialpath")
-        geo = pack_all.geometry()
-        materials = []
-        for mat in geo.primStringAttribValues("shop_materialpath"):
-            materials.append(mat)
-        # print(materials)
-        return materials
-
-    def create_materialx(self, mat, mat_lib):
-        shader = hou.node(mat.path() + "/principledshader1")
-        # print(shader.parm("basecolor_texture").evalAsString())
-        # parms = [parm for parm in shader.parms() if (parm.name().endswith("texture"))]
-        parms = []
-        for parm in shader.parms():
-            if parm.name().endswith("texture"):
-                if parm.evalAsString() != "":
-                    parms.append(parm)
-
-        mat_lib_path = hou.node(mat_lib.path())
-        mat_x = mat_lib_path.createNode("subnet", mat.name())
-        mat_x.setMaterialFlag(True)
-        output = hou.node(mat_x.path() + "/suboutput1")
-        mtlx_st_surface = output.createInputNode(0, "mtlxstandard_surface")
-
-        mtlx_diplacement = output.createInputNode(1, "mtlxdisplacement")
-
-        mat_properties = output.createInputNode(2, "kma_material_properties")
-
-        for parm in parms:
-            texture = hou.node(mat_x.path()).createNode("mtlximage")
-            try:
-                input = mtlx_st_surface.inputIndex(scheme[parm.name()])
-
-                mtlx_st_surface.setInput(input, texture)
-                texture.parm("file").set(parm.evalAsString())
-
-            except:
-                print("texture skiped {}".format(parm.evalAsString()))
-        mat_x.layoutChildren()
-
-    def create_maretial_library(self, stage_path):
-        mat_lib = hou.node(stage_path).createNode("materiallibrary")
-        return mat_lib
-
-    def build_materialx_library(self, node):
-
-        mat_lib = self.create_maretial_library(self.stage_path)
-
-        materials = self.get_material_data(node)
-        for mat in materials:
-            mat = hou.node(mat)
-
-            self.create_materialx(mat, mat_lib)
-        mat_lib.layoutChildren()
-        return mat_lib
-
-    def import_geo(self, node):
-        geo = self.read_geo_file(node)
-        geo = geo.split("/")[-1]
-        geo = geo.split(".")[0]
-        print(geo)
-
-        sop_import = hou.node(self.stage_path).createNode("sopimport", geo)  # temp find better way to source name
-        sop_import.parm("enable_partitionattribs").set(True)
-        sop_import.parm("partitionattribs").set("shop_materialpath")
-        sop_import.parm("soppath").set(node.path())
-        sop_import.parm("enable_pathattr").set(True)
-
-        prim = hou.node(self.stage_path).createNode("primitive")
-        prim.parm("primpath").set("/main")
-        prim.parm("primkind").set("assembly")
-
-        mat_lib = self.build_materialx_library(node)
-
-        graft_stages = hou.node(self.stage_path).createNode("graftstages")
-
-        graft_stages.setInput(0, prim)
-        graft_stages.setNextInput(sop_import)
-
-        graft_stages.setNextInput(mat_lib)
-
-        materials = self.get_material_data(node)
-        assign_mat = graft_stages.createOutputNode("assignmaterial")
-        assign_mat.parm("nummaterials").set(len(materials))
-        # mat_path should be in for loop
-
-        for mat in materials:
-            mat_path = (
-                prim.parm("primpath").evalAsString()
-                + "/"
-                + mat_lib.name()
-                + "/"
-                + "materials/"
-                + "/"
-                + mat.split("/")[-1]
-            )
-            prim_path = (
-                prim.parm("primpath").evalAsString()
-                + "/"
-                + geo
-                + "/"
-                + geo
-                + "/mesh_0/shop_materialpath_"
-                + mat.replace("/", "_")
-            )
-            assign_mat.parm("primpattern{}".format(materials.index(mat) + 1)).set(prim_path)
-            assign_mat.parm("matspecpath{}".format(materials.index(mat) + 1)).set(mat_path)
-            print(mat_path)
-
     def read_geo_file(self, node):
         queue = [node]
         while len(queue) > 0:
@@ -143,3 +30,142 @@ class KitBashGeometryImport:
                 return geo
             for node in current.inputs():
                 queue.append(node)
+    def get_geometry_data(self, node):
+        #open json file
+        with open(self.metadata, "r") as read_file:
+            read = json.load(read_file)
+
+
+        geometry_file = self.read_geo_file(node)
+
+
+        # Getting geo name
+        geo_name = geometry_file.split("/")[-1]
+        geo_name = geo_name.split(".")[0]
+
+        # Writing geomjetry file name and initializing textures dictionary
+        read[geometry_file] = {"asset_name": geo_name, "materials": {}}
+
+        # Packing geo based on material path
+        pack_all = node.createOutputNode("pack")  # thsi is why pack node is created twice
+        pack_all.parm("packbyname").set(True)
+        pack_all.parm("nameattribute").set("shop_materialpath")
+        pack_all.parm("transfer_attributes").set("shop_materialpath")
+        hou_geo = pack_all.geometry()
+
+        # Getting textures data
+        for mat in hou_geo.primStringAttribValues("shop_materialpath"):
+            shader = hou.node(mat + "/principledshader1")
+
+
+            mat_name = mat.split("/")[-1]
+            read[geometry_file]["materials"][mat_name]= {"shop_materialpath": mat, "textures" : {}}
+
+
+            for parm in shader.parms():
+                if parm.name().endswith("texture"):
+                    if parm.evalAsString() != "":
+                        read[geometry_file]["materials"][mat_name]["textures"][parm.name()] = parm.evalAsString()
+
+        # Writing json file
+        with open(self.metadata, "w") as output_file:
+            json.dump(read, output_file, indent=4)
+
+        # Pack remove
+        pack_all.destroy()
+
+
+    def create_main_template(self, geometry_file):
+        with open(self.metadata, "r") as read_file:
+            read = json.load(read_file)
+
+        # Create sop lop
+        sop_create = hou.node(self.stage_path).createNode("sopcreate", read[geometry_file]["asset_name"])  # change hardcoded name
+        sop_create.parm("enable_partitionattribs").set(True)
+        sop_create.parm("enable_pathattr").set(True)
+        # file sop
+
+        file_sop = hou.node(sop_create.path() + "/sopnet/create").createNode("file")
+        file_sop.parm("file").set(geometry_file)
+
+        # attrib wrangle
+        attrib_wrangle = file_sop.createOutputNode("attribwrangle")
+        attrib_wrangle.parm("class").set(1)
+        attrib_wrangle.parm("snippet").set(
+            's@path = "{}";'.format(read[geometry_file]["asset_name"])
+        )
+
+        # output
+        output_sop = attrib_wrangle.createOutputNode("output")
+        mat_lib = hou.node(self.stage_path).createNode("materiallibrary")
+        prim = hou.node(self.stage_path).createNode("primitive")
+        prim.parm("primpath").set("/main")
+        prim.parm("primkind").set("assembly")
+
+        graft_stages = hou.node(self.stage_path).createNode("graftstages")
+
+        graft_stages.setInput(0, prim)
+        graft_stages.setNextInput(sop_create)
+
+        graft_stages.setNextInput(mat_lib)
+
+        assign_mat = graft_stages.createOutputNode("assignmaterial")
+
+        self.create_materialx_library(geometry_file, mat_lib)
+        _materials = list(read[geometry_file]["materials"].keys())
+        assign_mat.parm("nummaterials").set(len(_materials))
+
+        for mat in _materials:
+            mat_path = (
+                    prim.parm("primpath").evalAsString()
+                    + "/"
+                    + mat_lib.name()
+                    + "/"
+                    + "materials"
+                    + "/"
+                    + mat
+            )
+            prim_path = (
+                    prim.parm("primpath").evalAsString()
+                    + "/"
+                    + sop_create.name()
+                    + "/"
+                    + sop_create.name()
+                    + "/"
+                    + read[geometry_file]["asset_name"] + "/shop_materialpath_"
+                    + read[geometry_file]["materials"][mat]["shop_materialpath"].replace("/", "_")
+            )
+            assign_mat.parm("primpattern{}".format(_materials.index(mat) + 1)).set(prim_path)
+            assign_mat.parm("matspecpath{}".format(_materials.index(mat) + 1)).set(mat_path)
+
+
+    def create_materialx_library(self, geometry_file, mat_lib):
+        mat_lib_path = hou.node(mat_lib.path())
+        with open(self.metadata, "r") as read_file:
+            read = json.load(read_file)
+        _materials = read[geometry_file]["materials"]
+        for mat in _materials:
+            mat_x = mat_lib_path.createNode("subnet",mat)
+            mat_x.setMaterialFlag(True)
+            output = hou.node(mat_x.path() + "/suboutput1")
+            mtlx_st_surface = output.createInputNode(0, "mtlxstandard_surface")
+
+            mtlx_diplacement = output.createInputNode(1, "mtlxdisplacement")
+
+            mat_properties = output.createInputNode(2, "kma_material_properties")
+
+            for texture in _materials[mat]["textures"]:
+
+                texture_node = hou.node(mat_x.path()).createNode("mtlximage")
+                try:
+
+                    input = mtlx_st_surface.inputIndex(scheme[texture])
+
+                    mtlx_st_surface.setInput(input, texture_node)
+                    texture_node.parm("file").set(_materials[mat]["textures"][texture])
+
+                except:
+                    print("texture skipped {}".format(format(_materials[mat]["textures"][texture])))
+            mat_x.layoutChildren()
+
+
